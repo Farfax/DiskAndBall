e9d6a0acde99b6b1e3d9030c48d57060c841b1f7
Farfax <mfarfax@gmail.com>
2018-05-05 18:38:30 +0200

    Update DefaultFuzzyLogic.cs

--- IntelligenceLib/DefaultFuzzyLogic.cs
+++ IntelligenceLib/DefaultFuzzyLogic.cs
@@ -1,63 +1,85 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace FuzzyLib
 {
-    public class InputContext
+// Use it when your principle is very simple (f.e: return value>0). Just give lambda/ function to the constructor.
+    public class ShortPrinciple: AbstractPrinciple
     {
-		//Fuck U Unity
-		//   private static readonly Tuple<float, float> ballSpeed = new Tuple<float, float>(-50, 50);
-		//  private static readonly Tuple<float, float> paddleSpeed = new Tuple<float, float>(-50, 50);
-		//  private static readonly Tuple<float, float> paddleRotationRange = new Tuple<float, float>(-180, 180);
-		// private static readonly Tuple<float, float> ballPositionRange = new Tuple<float, float>(-5,5);
-		private static readonly float ballSpeed = 50;
-		private static readonly float paddleSpeed = 50; 
-		private static readonly float paddleRotationRange = 180; 
-		private static readonly float ballPositionRange = 5;  
+        private Func<InputContext, Output, Output> prince;
 
-		public float[] ballVelocity { get; }
-        public float[] ballPosition { get; }
-        public float[] paddleRotation { get; }
-        public float[] paddleRotationVelocity { get;}
-
-        public InputContext(float[] ballVelocity = null, float[] ballPosition = null, float[] paddleRotation = null , float[] paddleRotationVelocity = null)
+        public ShortPrinciple( Func<InputContext,Output, Output> func)
         {
-            this.ballVelocity = ballVelocity ?? new float[3];
-            this.ballPosition = ballPosition ?? new float[3];
-            this.paddleRotation = paddleRotation ?? new float[3];
-            this.paddleRotationVelocity = paddleRotationVelocity ?? new float[3];
+            prince = func;
         }
 
-        private float[] convertVector(float range, float[] vector)
+        public Output apply(InputContext context, Output output)
         {
-            for (int i = 0; i < vector.Length; i++)
+            return prince(context, output);
+        }
+
+
+    }
+
+    public class DefaultPrinciple : AbstractPrinciple
+    {
+
+        public DefaultPrinciple()
+        {
+          
+        }
+
+        // Context is fuzzy value from graphic/physic, output is current response status.
+        public Output apply(InputContext context, Output output)
+        {
+        // here is our Principle logic
+            if(context.ballVelocity[0]>0.5)
             {
-				vector[i] /= range;
+				;
+            }
+            if (context.ballPosition[0] == context.ballVelocity[0])
+                output.paddleVelocity[0] = 1;
+
+            if (context.ballVelocity[0] == 1)
+                output.paddleVelocity[0] = 1;
+            return output;
+        }
+
+
+    }
+
+	//Usage, create InputContext object, then create DefaultLogic(context). Now u can get reponse object by use applyPrinciples().
+    public class DefaultLogic
+    {
+        List<AbstractPrinciple> principles;
+
+        InputContext context;
+
+        public DefaultLogic(InputContext context) 
+        {
+            this.context = context;
+            principles = new List<AbstractPrinciple>();
+            principles.Add(new DefaultPrinciple());
+          // here is place for Principles definitions
+        }
+		public DefaultLogic(float[] ballVelocity, float[] ballPosition, float[] paddleRotation, float[] paddleRotationVelocity)
+		{
+			this.context = new InputContext(ballVelocity, ballPosition, paddleRotation, paddleRotationVelocity).convertToFuzzy();
+			principles = new List<AbstractPrinciple>();
+			//  principles.Add(new DefaultPrinciple((con, output) => { re}));
+			// here is place for Principles definitions
+		}
+
+		public Output applyPrinciples()
+        {
+            Output output = new Output();
+
+            foreach(var principle in principles)
+            {
+                output = principle.apply(context, output);
             }
 
-            return vector;
-        }
-
-		// After conversion we have direct ( - or + ) and 'fuzzy strengh' 0-1 when 1 is max distance from point (0,0).
-        public InputContext convertToFuzzy()
-        {
-            return new InputContext(convertVector(ballSpeed, ballVelocity),convertVector(ballPositionRange, ballPosition), convertVector(paddleRotationRange, paddleRotation), convertVector(paddleSpeed, paddleRotationVelocity));   
+            return output;
         }
     }
-
-    public class Output
-    {
-        public float[] paddleVelocity { get; set;  }
-
-        public Output(float[] paddleVelocity= null)
-        {
-            this.paddleVelocity = paddleVelocity ?? new float[3];
-        }
-    }
-
-    interface AbstractPrinciple
-    {
-    Output apply(InputContext context, Output output);
-    }
-}
+	}
